309878b17f29276428081d723fb543529c93248e
Taylor Ivanoff <taylorivanoff@gmail.com>
2024-05-10 23:43:44 +1000

    Update main.js

--- main.js
+++ main.js
@@ -31,15 +31,6 @@ app.on('ready', () => {
 
   mainWindow.loadURL('https://www.icloud.com/notes');
 
-  // Specify user agent to prevent "download iCloud for Windows" banner
-  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15";
-    callback({
-      cancel: false,
-      requestHeaders: details.requestHeaders
-    });
-  });
-
   // Listen for authentication-related cookies
   session.defaultSession.cookies.on('changed', (event, cookie, cause, removed) => {
     if (!removed && cookie.domain.includes('.icloud.com')) {
